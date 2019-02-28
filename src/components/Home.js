626cd7163f49caed0bb39dd1ea4a8a6ea8127bc2
Michael Stelly <mjstelly@gmail.com>
2019-02-27 20:45:43 -0500

    wip: added navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,24 @@ Date: 27 Feb 2019
 
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import { Container } from 'native-base'
+import { Button, Container } from 'native-base'
 
-const Home = () => {
-  return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
-  )
+class Home extends Component {
+  render() {
+    const { navigate } = this.props.navigation
+    return (
+      <Container
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Button primary onPress={() => navigate('AddReservation')}>
+          <Text>Add Reservation</Text>
+        </Button>
+        <Button primary onPress={() => navigate('ListReservations')}>
+          <Text>List Reservations</Text>
+        </Button>
+      </Container>
+    )
+  }
 }
 
 export default Home
