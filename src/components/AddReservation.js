33a55abc50812336086a06635f6f7901272b1b6c
Michael Stelly <mjstelly@gmail.com>
2019-03-01 19:43:56 -0500

    wip: adding mutation code

--- src/components/AddReservation.js
+++ src/components/AddReservation.js
@@ -17,6 +17,8 @@ Date: 20 Feb 2019
 import React, { Component } from 'react'
 import { StyleSheet, Text } from 'react-native'
 import { Container, Content, DatePicker, Form, Item, Input } from 'native-base'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 
 const styles = StyleSheet.create({})
 
@@ -39,6 +41,23 @@ const DateSelector = () => {
   )
 }
 
+reservation = {
+  name: '',
+  hotelName: '',
+  arrivalDate: '',
+  departureDate: ''
+}
+const CREATE_RESERVATION = gql`
+  mutation createReservation($res: ReservationCreateInput!) {
+    createReservation(data: $res) {
+      name
+      hotelName
+      arrivalDate
+      departureDate
+    }
+  }
+`
+
 class AddReservation extends Component {
   state = {
     chosenDate: new Date()
@@ -50,20 +69,30 @@ class AddReservation extends Component {
     return (
       <Container>
         <Content>
-          <Form>
-            <Item>
-              <Input placeholder="Name" />
-            </Item>
-            <Item last>
-              <Input placeholder="Hotel Name" />
-            </Item>
-            <Item>
-              <DateSelector />
-            </Item>
-            <Item last>
-              <DateSelector />
-            </Item>
-          </Form>
+          <Mutation mutation={CREATE_RESERVATION}>
+            {(createReservation, { data })} => (
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault()
+                createReservation({ variables: { type: input.value } })
+                input.value = ''
+              }}
+            >
+              <Item>
+                <Input placeholder="Name" />
+              </Item>
+              <Item last>
+                <Input placeholder="Hotel Name" />
+              </Item>
+              <Item>
+                <DateSelector />
+              </Item>
+              <Item last>
+                <DateSelector />
+              </Item>
+            </Form>
+            )
+          </Mutation>
         </Content>
       </Container>
     )
